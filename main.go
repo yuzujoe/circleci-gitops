2fddabb5e41b6f8c6b30df837318f7fbdf80064c
yuzujoe <yuzun225@gmail.com>
2020-08-23 01:34:31 +0900

    add path

--- main.go
+++ main.go
@@ -9,6 +9,10 @@ import (
 func main() {
 	r := gin.Default()
 
+	r.GET("/", func(c *gin.Context) {
+		c.JSON(200, "Hello Argo Rollout")
+	})
+
 	r.GET("/app-a", func(c *gin.Context) {
 		c.JSON(200, "Hello World")
 	})
